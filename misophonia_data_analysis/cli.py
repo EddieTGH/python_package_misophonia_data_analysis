5457259066fdf786f52bf398e6e71dd5114ef8fa
EddieTGH <edmond.niu@gmail.com>
2024-04-25 18:16:33 -0400

    [DEV] updated CLI now assumes personalized input files

--- misophonia_data_analysis/cli.py
+++ misophonia_data_analysis/cli.py
@@ -1,12 +1,21 @@
 r"""Title: Process Sound Task Raw Data From Qualtrics
 
+Setup:
+------
+1. Ensure that the raw data file is in the correct location:
+- The raw data file should be named as 'miso_raw_{subject_number}.csv'
+- The raw data file should be placed in the 'input_data/raw_data' folder
+
+2. Ensure that the mapping data file is in the correct location:
+- The mapping data file should be named as 'miso_mapping_{subject_number}.csv'
+- The mapping data file should be placed in the 'input_data/mapping' folder
+
+3. Ensure that the output_data folder exists in the root directory
+
 Examples
 --------
 run_miso -s 1001
 run_miso -s 1023
-run_miso --raw-data-path ../../input_data/raw_data/miso_raw_data_9.csv -s 1001
-run_miso --mapping-data-path ../../input_data/mapping/miso_mapping_sounds_1.csv -s 1001
-run_miso --raw-data-path ../../input_data/raw_data/miso_raw_data_9.csv --mapping-data-path ../../input_data/mapping/miso_mapping_sounds_1.csv -s 1001
 
 """
 
@@ -35,28 +44,6 @@ def _get_args():
         formatter_class=RawTextHelpFormatter,
     )
 
-    parser.add_argument(
-        "--raw-data-path",
-        type=str,
-        default="../../input_data/raw_data/miso_raw_data_13.csv",
-        help=textwrap.dedent(
-            """\
-            (default : %(default)s)
-            """
-        ),
-    )
-
-    parser.add_argument(
-        "--mapping-data-path",
-        type=str,
-        default="../../input_data/mapping/miso_mapping_sounds_2.csv",
-        help=textwrap.dedent(
-            """\
-            (default : %(default)s)
-            """
-        ),
-    )
-
     required_args = parser.add_argument_group("Required Arguments")
     required_args.add_argument(
         "-s",
@@ -80,13 +67,36 @@ def main():
     # Capture CLI arguments
     args = _get_args().parse_args()
     subjN = args.subject_number
-    raw_data_path = args.raw_data_path
-    mapping_data_path = args.mapping_data_path
+    raw_data_path = f"../../input_data/raw_data/miso_raw_{subjN}.csv"
+    mapping_data_path = f"../../input_data/mapping/miso_mapping_{subjN}.csv"
 
+    # Check if the subject_number is an integer
+    try:
+        args.subject_number = int(args.subject_number)
+    except ValueError:
+        print("Subject ID must be an integer")
+        sys.exit(1)
+
+
+    #make sure file exists in the right location
+    # Check if the raw data file exists at the given path
+    if not os.path.exists(raw_data_path):
+        raw_data_path = raw_data_path[3:]
+
+    if not os.path.exists(raw_data_path):
+        print(f"Error: The raw data file does not exist at {raw_data_path}", file=sys.stderr)
+        sys.exit(1)  # Exit the program with an error code
+
+    if not os.path.exists(mapping_data_path):
+        mapping_data_path = mapping_data_path[3:]
+        
+    # Check if the mapping data file exists at the given path
+    if not os.path.exists(mapping_data_path):
+        print(f"Error: The mapping data file does not exist at {mapping_data_path}", file=sys.stderr)
+        sys.exit(1)  # Exit the program with an error code
+
+    # If the code execution reaches this point, it means both files exist and the program can proceed
     SoundDataAnalysis.proc_data(subjN, raw_data_path, mapping_data_path)
-
-    #make sure file exists in the right location, make sure the subject number is available
-
 
 if __name__ == "__main__":
     main()
