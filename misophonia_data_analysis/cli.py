19f3293631213ed38e6d967e95342233305d0a07
EddieTGH <edmond.niu@gmail.com>
2024-04-26 16:14:05 -0400

    [DEV] Prepare for munin/biac data structure

--- misophonia_data_analysis/cli.py
+++ misophonia_data_analysis/cli.py
@@ -1,39 +1,52 @@
-r"""Title: Process Sound Task Raw Data From Qualtrics
+r"""Title: Process Sound Task Raw Data From Qualtrics (for BIAC/munin)
 
-Utilize this script to process raw data from 'pre' and 'post' sound tasks...
-'pre' refers to data acquired from the initial sound task
-'post' refers to data acquired from the follow-up sound task
+Utilize this script to process raw data from intake and follow up sound tasks...
+'intake' refers to data acquired from the initial sound task
+'fu_1month' refers to data acquired from the 1 month follow-up sound task
+'fu_3month' refers to data acquired from the 3 month follow-up sound task
 
-'Pre' Setup:
+'Intake' Setup:
 ------
 1. Ensure that the raw data file is in the correct location:
-- The raw data file should be named as 'miso_raw_{subject_number}.csv'
-- The raw data file should be placed in the 'input_data/raw_data' folder
+- The Qualtrics intake raw data file should be named as 'raw_qualtrics_intake_{subjN}.csv'
+- The Qualtrics intake raw data file should be placed in the 'data/task_sound/raw_intake' folder
 
 2. Ensure that the mapping data file is in the correct location:
-- The mapping data file should be named as 'miso_mapping_{subject_number}.csv'
-- The mapping data file should be placed in the 'input_data/mapping' folder
-
-3. Ensure that the output_data folder exists in the root directory
+- The mapping intake data file should be named as 'mapping_intake_{subjN}.csv'
+- The mapping intake data file should be placed in the 'data/task_sound/mapping_intake' folder
 
 
-'Post' Setup:
+'fu_1month' Setup:
 ------
-1. Ensure that 'pre' data analysis has already been run (output files generated).
-- This should ensure that the mapping data file is available for the 'post' data analysis
+1. Ensure that intake data analysis has already been run (output files generated).
+- This should ensure that the mapping data file is available for the follow up data analysis
+- The mapping data file should be found in the 'data/task_sound/mapping_fu' folder
+- The mapping data file should be named as 'subject_{subjN}_follow_up_mapping.csv'
 
-2. Ensure that the raw data file is in the correct location:
-- The raw data file should be named as 'miso_raw_post_{subject_number}.csv'
-- The raw data file should be placed in the 'input_data/raw_post_data' folder
+2. Ensure that the Qualtrics fu raw data file is in the correct location:
+- The Qualtrics fu raw data file should be named as 'raw_qualtrics_fu_1-month_{subjN}.csv'
+- The Qualtrics fu raw data file should be placed in the 'data/task_sound/raw_fu_1-month' folder
 
-3. Ensure that the output_data folder exists in the root directory
+
+'fu_3month' Setup:
+------
+1. Ensure that intake data analysis has already been run (output files generated).
+- This should ensure that the mapping data file is available for the follow up data analysis
+- The mapping data file should be found in the 'data/task_sound/mapping_fu' folder
+- The mapping data file should be named as 'subject_{subjN}_follow_up_mapping.csv'
+
+2. Ensure that the Qualtrics fu raw data file is in the correct location:
+- The Qualtrics fu raw data file should be named as 'raw_qualtrics_fu_3-month_{subjN}.csv'
+- The Qualtrics fu raw data file should be placed in the 'data/task_sound/raw_fu_3-month' folder
 
 Examples
 --------
-run_miso -s 1001 -t pre
-run_miso -s 1001 -t post
-run_miso -s 1023 -t pre
-run_miso -s 1001 -t post
+python cli.py -s 1001 -t intake
+python cli.py -s 1001 -t fu_1month
+python cli.py -s 1001 -t fu_3month
+python cli.py -s 1023 -t intake
+python cli.py -s 1001 -t fu_1month
+python cli.py -s 1001 -t fu_3month
 
 """
 
@@ -73,7 +86,7 @@ def _get_args():
 
     required_args.add_argument(
         "-t",
-        help="Type of processing: 'pre' for initial sound task or 'post' for follow-up task",
+        help="Type of processing: 'intake' for initial sound task or 'fu_1month' or 'fu_3month for follow-up task",
         type=str,
         required=True,
         dest="type_processing"
@@ -94,14 +107,18 @@ def main():
     args = _get_args().parse_args()
     subjN = args.subject_number
     type_processing = args.type_processing
-    raw_data_path = f"../../input_data/raw_data/miso_raw_{subjN}.csv"
-    mapping_data_path = f"../../input_data/mapping/miso_mapping_{subjN}.csv"
 
-    post_data_path = f"../../input_data/raw_post_data/miso_raw_post_{subjN}.csv"
-    #post_data_path = f"../../input_data/raw_post_data/miso_raw_post_25.csv"
+    task_sound_dir = "/mnt/munin/Neacsiu/MISOSTIM.01/Data/task_sound"
+    raw_intake = task_sound_dir + f"/raw_intake/raw_qualtrics_intake_{subjN}.csv"
+    mapping_intake = task_sound_dir + f"/mapping_intake/mapping_intake_{subjN}.csv"
+    #raw_data_path = f"../../input_data/raw_data/miso_raw_{subjN}.csv"
+    #mapping_data_path = f"../../input_data/mapping/miso_mapping_{subjN}.csv"
 
-    post_mapping_data_path = f"../../output_data/subject_{subjN}/subject_{subjN}_follow_up_mapping.csv"
-    #post_mapping_data_path = f"../../output_data/subject_25/subject_25_follow_up_mapping.csv"
+    raw_fu_1month = task_sound_dir + f"/raw_fu_1-month/raw_qualtrics_fu_1-month_{subjN}.csv"
+    raw_fu_3month = task_sound_dir + f"/raw_fu_3-month/raw_qualtrics_fu_3-month_{subjN}.csv"
+    mapping_fu = task_sound_dir + f"/mapping_fu/mapping_fu_{subjN}.csv"
+    #post_data_path = f"../../input_data/raw_post_data/miso_raw_post_{subjN}.csv"
+    #post_mapping_data_path = f"../../output_data/subject_{subjN}/subject_{subjN}_follow_up_mapping.csv"
 
 
     # Check if the subject_number is an integer
@@ -111,50 +128,71 @@ def main():
         print("Subject ID must be an integer")
         sys.exit(1)
 
-    if type_processing == 'pre':
+    if type_processing == 'intake':
         #make sure file exists in the right location
         # Check if the raw data file exists at the given path
-        if not os.path.exists(raw_data_path):
-            raw_data_path = raw_data_path[3:]
+        if not os.path.exists(raw_intake):
+            raw_intake = raw_intake[3:]
 
-        if not os.path.exists(raw_data_path):
-            print(f"Error: The 'pre' raw data file does not exist at {raw_data_path}", file=sys.stderr)
+        if not os.path.exists(raw_intake):
+            print(f"Error: The 'intake' raw data file does not exist at {raw_intake}", file=sys.stderr)
             sys.exit(1)  # Exit the program with an error code
 
-        if not os.path.exists(mapping_data_path):
-            mapping_data_path = mapping_data_path[3:]
+        if not os.path.exists(mapping_intake):
+            mapping_intake = mapping_intake[3:]
 
         # Check if the mapping data file exists at the given path
-        if not os.path.exists(mapping_data_path):
-            print(f"Error: The 'pre' mapping data file does not exist at {mapping_data_path}", file=sys.stderr)
+        if not os.path.exists(mapping_intake):
+            print(f"Error: The 'intake' mapping data file does not exist at {mapping_intake}", file=sys.stderr)
             sys.exit(1)  # Exit the program with an error code
 
         # If the code execution reaches this point, it means both files exist and the program can proceed
-        SoundDataAnalysis.proc_data(subjN, raw_data_path, mapping_data_path, type_processing)
+        SoundDataAnalysis.proc_intake(subjN, raw_intake, mapping_intake)
 
-    elif type_processing == 'post':
+    elif type_processing == 'fu_1month':
         #make sure file exists in the right location
         # Check if the raw data file exists at the given path
-        if not os.path.exists(post_data_path):
-            post_data_path = post_data_path[3:]
+        if not os.path.exists(raw_fu_1month):
+            raw_fu_1month = raw_fu_1month[3:]
 
-        if not os.path.exists(post_data_path):
-            print(f"Error: The post raw data file does not exist at {post_data_path}", file=sys.stderr)
+        if not os.path.exists(raw_fu_1month):
+            print(f"Error: The 1 month follow up Qualtrics raw data does not exist at {raw_fu_1month}", file=sys.stderr)
             sys.exit(1)
         
-        if not os.path.exists(post_mapping_data_path):
-            post_mapping_data_path = post_mapping_data_path[3:]
+        if not os.path.exists(mapping_fu):
+            mapping_fu = mapping_fu[3:]
 
         # Check if the mapping data file exists at the given path
-        if not os.path.exists(post_mapping_data_path):
-            print(f"Error: The post mapping data file does not exist at {post_mapping_data_path}. Please make sure to run 'pre' data processing first.", file=sys.stderr)
+        if not os.path.exists(mapping_fu):
+            print(f"Error: The follow up mapping data file does not exist at {mapping_fu}. Please make sure to run intake data processing first.", file=sys.stderr)
             sys.exit(1)
         
         # If the code execution reaches this point, it means both files exist and the program can proceed
-        SoundDataAnalysis.proc_data(subjN, post_data_path, post_mapping_data_path, type_processing)
+        SoundDataAnalysis.proc_fu_1month(subjN, raw_fu_1month, mapping_fu)
     
+    elif type_processing == 'fu_3month':
+        #make sure file exists in the right location
+        # Check if the raw data file exists at the given path
+        if not os.path.exists(raw_fu_3month):
+            raw_fu_3month = raw_fu_3month[3:]
+
+        if not os.path.exists(raw_fu_3month):
+            print(f"Error: The 3 month follow up Qualtrics raw data does not exist at {raw_fu_3month}", file=sys.stderr)
+            sys.exit(1)
+        
+        if not os.path.exists(mapping_fu):
+            mapping_fu = mapping_fu[3:]
+
+        # Check if the mapping data file exists at the given path
+        if not os.path.exists(mapping_fu):
+            print(f"Error: The follow up mapping data file does not exist at {mapping_fu}. Please make sure to run intake data processing first.", file=sys.stderr)
+            sys.exit(1)
+        
+        # If the code execution reaches this point, it means both files exist and the program can proceed
+        SoundDataAnalysis.proc_fu_3month(subjN, raw_fu_3month, mapping_fu)
+
     else:
-        print("Invalid type of processing. Please enter 'pre' or 'post'")
+        print("Invalid type of processing. Please enter 'intake', 'fu_1month' or 'fu_3month'.")
         sys.exit(1)
 
 if __name__ == "__main__":
